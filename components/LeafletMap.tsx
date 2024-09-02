749ebcccccbefe7ab3a907b4dc184f584b762dda
gunsow <fukudatatsuya911@gmail.com>
2024-09-02 21:08:49 +0900

    leafletによる画面作成

---
+++ components/LeafletMap.tsx
@@ -0,0 +1,16 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+
+export default function LeafletMap() {
+  return (
+    <MapContainer
+      style={{ width: "100%", height: "100dvh" }}
+      center={{ lat: 34.18583, lng: 131.47139 }}
+      zoom={13}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
