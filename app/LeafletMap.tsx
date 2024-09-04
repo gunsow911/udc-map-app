03b3b937b28115e55be2ec7b3fadaf58b6009f13
gunsow <fukudatatsuya911@gmail.com>
2024-09-05 00:00:07 +0900

    サンプル用のマーカーを設置

--- app/LeafletMap.tsx
+++ app/LeafletMap.tsx
@@ -1,5 +1,7 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+L.Icon.Default.imagePath = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/";
 
 export default function LeafletMap() {
   return (
@@ -12,6 +14,9 @@ export default function LeafletMap() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={{ lat: 34.18583, lng: 131.47139 }}>
+        <Popup>マーカーテストです。</Popup>
+      </Marker>
     </MapContainer>
   );
 }
